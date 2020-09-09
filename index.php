b405db2fa8ef987388930ca3a82c4c8d3d3c0750
Leonel Henriquez <leonelhenriquez@users.noreply.github.com>
2020-09-09 00:32:03 -0600

    Index Update

--- index.php
+++ index.php
@@ -1,22 +1,25 @@
 <?php
-	header("Content-Type: text/html; charset=UTF-8");
-	define('__DEV_MODE__', true);
-	define('__PATH_SOURCE__', __DIR__.'/app');
-
-
-	//var_dump(session_get_cookie_params());
-	//exit();
-
-	require_once __PATH_SOURCE__."/Config.php";
-	require_once __PATH_SOURCE__."/App.php";
-	$app = new app\App();
-
+	
 	/*
-	 * Version : 4.11.02.19
+	 * Version : 5.2020.09.09
 	 *
 	 * @author Leonel Henriquez
 	 * (C) Leonel Henriquez
 	 */
-	$app;
-	unset($app);
+
+	header("Content-Type: text/html; charset=UTF-8");
+	define('__DEV_MODE__', true);
+	define('__PATH_SOURCE__', __DIR__.'/app');
+
+	//var_dump(session_get_cookie_params());
+
+	require_once __PATH_SOURCE__."/Config.php";
+	require_once __PATH_SOURCE__."/App.php";
+	
+
+	
+	(new app\App());
+	
+
+
 ?>
